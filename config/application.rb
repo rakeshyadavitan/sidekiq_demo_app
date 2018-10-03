e1082d0b27209868902bf2fdd162c8e2de0a457f
Rakesh Yadav <rakeshkumar_y@hcl.com>
2018-10-03 16:17:25 +0530

    application.rb file update

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module SidekiqRailsDemo
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
 
     config.active_job.queue_adapter = :sidekiq
   end
